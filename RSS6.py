a304af3d0cd8b348487e1ff72daeb135fb8d3f69
aiueo0306 <sato.sota@create-sd.co.jp>
2025-07-25 19:08:19 +0900

    Update RSS6.py

--- RSS6.py
+++ RSS6.py
@@ -5,8 +5,8 @@ import os
 import re
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
-BASE_URL = "https://www.jslsd.jp/news/"
-GAKKAI = "日本腰痛学会"
+BASE_URL = "https://www.js-smid.org/news/"
+GAKKAI = "日本重症心身障害学会"
 
 def generate_rss(items, output_path):
     fg = FeedGenerator()
@@ -34,9 +34,9 @@ def generate_rss(items, output_path):
 
 def extract_items(page):
 
-    page.wait_for_selector("section.sec-news h3", timeout=10000) 
+    page.wait_for_selector("div.wrapper li", timeout=10000) 
     
-    selector = "div.newsList > a"
+    selector = "div.wrapper li"
     blocks = page.locator(selector)
     count = blocks.count()
     
@@ -51,10 +51,10 @@ def extract_items(page):
             # 🕒 日付を現在時刻に固定
             pub_date = datetime.now(timezone.utc)
 
-            title = block.locator("h3.title").inner_text().strip()
+            title = block.locator("p").inner_text().strip()
 
             try:
-                href = block.get_attribute("href")
+                href = block.locate("a).get_attribute("href")
                 full_link = urljoin(BASE_URL, href)
             except:
                 href = ""
