2ca1ca51c72dfbbb4b699e5c3d2de662a9b21eae
aiueo0306 <sato.sota@create-sd.co.jp>
2025-07-25 20:09:58 +0900

    Update RSS9.py

--- RSS9.py
+++ RSS9.py
@@ -5,8 +5,8 @@ import os
 import re
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
-BASE_URL = "http://www.praj.jp/topics/list.php"
-GAKKAI = "日本小児リウマチ学会"
+BASE_URL = "https://www.memai.jp/category/news/"
+GAKKAI = "日本めまい平衡医学会"
 
 def generate_rss(items, output_path):
     fg = FeedGenerator()
@@ -34,9 +34,9 @@ def generate_rss(items, output_path):
 
 def extract_items(page):
 
-    page.wait_for_selector("div.topics li", timeout=10000) 
+    page.wait_for_selector("div#main_content li", timeout=10000) 
     
-    selector = "div.topics li"
+    selector = "div#main_content li"
     blocks = page.locator(selector)
     count = blocks.count()
     print(f"📦 発見した記事数: {count}")
