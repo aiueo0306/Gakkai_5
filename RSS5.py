d3c76b8165a1ac3956fdc0db2233cf78b3d437eb
aiueo0306 <sato.sota@create-sd.co.jp>
2025-07-25 15:43:52 +0900

    Update RSS5.py

--- RSS5.py
+++ RSS5.py
@@ -5,8 +5,8 @@ import os
 import re
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
-BASE_URL = "https://www.jspaci.jp/news/"
-GAKKAI = "日本小児アレルギー学会"
+BASE_URL = "https://dementia-japan.org/category/news/"
+GAKKAI = "日本認知症学会"
 
 def generate_rss(items, output_path):
     fg = FeedGenerator()
@@ -34,9 +34,9 @@ def generate_rss(items, output_path):
 
 def extract_items(page):
 
-    page.wait_for_selector("ul.news-list li", timeout=10000) 
+    page.wait_for_selector("div#main_content li", timeout=10000) 
     
-    selector = "ul.news-list li"
+    selector = "div#main_content li"
     blocks = page.locator(selector)
     count = blocks.count()
     print(f"📦 発見した記事数: {count}")
