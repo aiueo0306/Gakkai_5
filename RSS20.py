2e8064e8056a4d316363521d077e79627a5baf18
aiueo0306 <sato.sota@create-sd.co.jp>
2025-07-26 14:23:14 +0900

    Update RSS20.py

--- RSS20.py
+++ RSS20.py
@@ -5,8 +5,8 @@ import os
 import re
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
-BASE_URL = "https://www.jsnt.gr.jp/"
-GAKKAI = "日本神経治療学会"
+BASE_URL = "https://www.perio.jp/news/"
+GAKKAI = "日本歯周病学会"
 
 def generate_rss(items, output_path):
     fg = FeedGenerator()
@@ -34,9 +34,9 @@ def generate_rss(items, output_path):
 
 def extract_items(page):
 
-    page.wait_for_selector("div.info dl", timeout=10000) 
+    page.wait_for_selector("div.section li", timeout=10000) 
     
-    selector = "div.info dl"
+    selector = "div.section li"
     blocks = page.locator(selector)
     count = blocks.count()
     print(f"📦 発見した記事数: {count}")
@@ -50,7 +50,7 @@ def extract_items(page):
             # 🕒 日付を現在時刻に固定
             pub_date = datetime.now(timezone.utc)
 
-            title = block.locator("span").first.inner_text().strip()
+            title = block.locator("a").first.inner_text().strip()
                 
             try:
                 href = block.locator("a").first.get_attribute("href")
