def416b9388e7724e3d41285c2efb37136755fa1
aiueo0306 <sato.sota@create-sd.co.jp>
2025-07-26 10:11:12 +0900

    Update RSS13.py

--- RSS13.py
+++ RSS13.py
@@ -5,8 +5,8 @@ import os
 import re
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
-BASE_URL = "https://www.jscn.gr.jp/"
-GAKKAI = "日本臨床栄養学会"
+BASE_URL = "https://www.jscpt.jp/info"
+GAKKAI = "日本臨床薬理学会"
 
 def generate_rss(items, output_path):
     fg = FeedGenerator()
@@ -34,9 +34,9 @@ def generate_rss(items, output_path):
 
 def extract_items(page):
 
-    page.wait_for_selector("div.margin-l8 li", timeout=10000) 
+    page.wait_for_selector("ul.info_list li", timeout=10000) 
     
-    selector = "div.margin-l8 li"
+    selector = "ul.info_list li"
     blocks = page.locator(selector)
     count = blocks.count()
     print(f"📦 発見した記事数: {count}")
