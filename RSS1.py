a861ce0dd1f9a5643c5ae3b926ea6ed983a5525f
aiueo0306 <sato.sota@create-sd.co.jp>
2025-08-03 13:27:54 +0900

    Update RSS1.py

--- RSS1.py
+++ RSS1.py
@@ -50,7 +50,7 @@ def extract_items(page):
             # 🕒 日付を現在時刻に固定
             
             date_text = block.locator("div.date.col-4.col-md-2").inner_text().strip()
-            pub_date = datetime.strptime(date_text, "%Y-%m-%d").replace(tzinfo=timezone.utc)
+            pub_date = parser.parse(date_text).replace(tzinfo=timezone.utc)
             
             title = block.locator("a").first.inner_text().strip()
                 
